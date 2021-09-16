175d3e5506c2c23d0bb0d692191a32cf11fc2ba6
Shadi <31284685+shadifuladi@users.noreply.github.com>
2021-09-16 15:22:52 -0500

    Update app.py
    up

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from bokeh.plotting import figure
-from bokeh.embed import components
-import pandas as pd
 import requests
 from flask import Flask, render_template, request, redirect
 
@@ -13,7 +10,6 @@ def myFetch(ticker):
    API_KEY = 'JOLFH26C08JHRMJ8'
    stock_name = 'AAPL'
    r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stock_name + '&apikey=' + API_KEY)
-
    result = r.json()	
    df = pd.DataFrame(result['Time Series (Daily)'])
    return df
@@ -25,10 +21,8 @@ def myFetch(ticker):
 def myPlot(df, sel, ticker):
     p = figure(title="AAPL Prices", x_axis_type="datetime", x_axis_label="Date",
                y_axis_label="Stock price", plot_width=1000)
-
-    mapping = {'open': 'open', 'close: 'close', 'high': 'high', 'low': 'low', 'volume': 'volume'}
-    colour = {'open': 'orange', 'close': 'red', 'high': 'blue', 'low': 'green', 'volume': 'yellow'}
-
+    mapping = {'open': 'open', 'adjOpen': 'adj. open', 'close': 'close', 'adjClose': 'adj. close'}
+    colour = {'open': 'orange', 'adjOpen': 'red', 'close': 'blue', 'adjClose': 'green'}
     for s in sel:
         p.line(df.index, df[mapping[s]], color=colour[s], legend=ticker + ": " + mapping[s])
     return p
@@ -44,28 +38,18 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return redirect('/index')
-
-
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-
 @app.route('/graph', methods=['GET', 'POST'])
 def graph():
     ticker = request.form['ticker']
     ticker = ticker.upper()
     sel = request.form.getlist('priceType')
-
     data = myFetch(ticker)
     plot = myPlot(data, sel, ticker)
-
     script, div = components(plot)
     reqUrl = "https://www.google.com/finance?q=" + ticker
     return render_template('graph.html', script=script, div=div, reqUrl=reqUrl)
-
-
 if __name__ == '__main__':
-  #app.run(port=33097)
-  const PORT = process.env.PORT
-  app.set("port", PORT)
-  app.run(debug=True)
+  app.run(port=33507)
