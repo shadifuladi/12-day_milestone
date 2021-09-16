0d4e23ae58464f1055f7ff9db0b2d0dcd3c0f841
Shadi <31284685+shadifuladi@users.noreply.github.com>
2021-09-16 14:54:09 -0500

    Update app.py
    up

--- app.py
+++ app.py
@@ -3,54 +3,61 @@ from bokeh.embed import components
 import pandas as pd
 import requests
 from flask import Flask, render_template, request, redirect
-from alpha_vantage.timeseries import TimeSeries
 
 
-def myFetch(ticker, price):
+#########################
+# get data:
+#########################
+
+def myFetch(ticker):
    API_KEY = 'JOLFH26C08JHRMJ8'
-   # stock_name = 'AAPL'
-   r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ticker + '&apikey=' + API_KEY)
-   
-   #ts = TimeSeries(API_KEY, output_format="pandas", indexing_type="integer)
-   #data, meta_data = ts.get_monthly_adjusted(symbol=ticker.upper())
+   stock_name = 'AAPL'
+   r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stock_name + '&apikey=' + API_KEY)
 
    result = r.json()	
    df = pd.DataFrame(result['Time Series (Daily)'])
    return df
 
-def myPlot(df, price, ticker):
-    p = figure(title="This is the plot!", x_axis_type="datetime", x_axis_label="Date",
+#########################
+# myPlot function, plots the data!
+#########################
+
+def myPlot(df, sel, ticker):
+    p = figure(title="AAPL Prices", x_axis_type="datetime", x_axis_label="Date",
                y_axis_label="Stock price", plot_width=1000)
 
-    mapping = {'open': 'open', 'adjOpen': 'adj. open', 'close': 'close', 'adjClose': 'adj. close'}
-    colour = {'open': 'orange', 'adjOpen': 'red', 'close': 'blue', 'adjClose': 'green'}
+    mapping = {'open': 'open', 'close: 'close', 'high': 'high', 'low': 'low', 'volume': 'volume'}
+    colour = {'open': 'orange', 'close': 'red', 'high': 'blue', 'low': 'green', 'volume': 'yellow'}
 
-    for p in price:
+    for s in sel:
         p.line(df.index, df[mapping[s]], color=colour[s], legend=ticker + ": " + mapping[s])
     return p
 
-app = Flask(__name__)
-app.vars = {}
 
-@app.route('/', methods=['GET', 'POST'])
+
+#########################
+# build the app via flask
+#########################
+
+app = Flask(__name__)
+
+@app.route('/')
 def main():
     return redirect('/index')
 
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-# if request.method == 'GET':
     return render_template('index.html')
-# else:
 
 @app.route('/graph', methods=['GET', 'POST'])
 def graph():
-    app.vars['ticker'] = request.form['ticker']
-    #ticker = ticker.upper()
-    app.vars['price'] = request.form.getlist('priceType')
+    ticker = request.form['ticker']
+    ticker = ticker.upper()
+    sel = request.form.getlist('priceType')
 
     data = myFetch(ticker)
-    plot = myPlot(data, price, ticker)
+    plot = myPlot(data, sel, ticker)
 
     script, div = components(plot)
     reqUrl = "https://www.google.com/finance?q=" + ticker
